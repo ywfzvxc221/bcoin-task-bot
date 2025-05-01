8939815c401703a10c2bc6cad23b2de8b5c7439e
ywfzvxc221 <sscccrr12@gmail.com>
2025-05-01 21:44:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "7671969883:AAElIjOGnNeYgNapLrivHImJFlPCNuYmMFQ"
+ADMIN_ID = 5475256932  # ضع هنا آيدي حسابك بدون @
+CHANNELS = [
+    "https://t.me/qq122311w",
+    "https://t.me/tayfahsiwids",
+    "https://t.me/qqww12ew",
+]
